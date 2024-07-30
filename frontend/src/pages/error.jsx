10d04606a095c5aab6c8796e28183f098722ea9f
Flash191102 <79019475+Flash191102@users.noreply.github.com>
2024-07-30 13:24:36 +0530

    first commit

--- frontend/src/pages/error.jsx
+++ frontend/src/pages/error.jsx
@@ -2,7 +2,7 @@ function Error(){
     return<div>
             <h1>404 Not Found</h1>
             <p>Your visited page not found. You may go to home page.</p>
-            <button><a href='/login'>Back to home page</a></button>
+            <button><a href='/'>Back to home page</a></button>
         </div>
     }
 export default Error;
