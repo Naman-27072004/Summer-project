42bc87202e267cb3c0dd2a1a13183e4c98d3fee9
Naman-27072004 <namangupta27072004@gmail.com>
2024-07-31 12:48:46 +0530

    first commit

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -1,22 +1,43 @@
+import '../Styles/contact.css'
 function Contact(){
-    return<div>
-            <div>
-                <i></i>
-                <h5>Call To Us</h5>
-                <p>We are avialable 24/7, 7 days a week</p>
-                <p>phone: </p>
-                <i></i>
-                <p>Fill out our form we will contact you with in 24 hours.</p>
-                <p>Emails: customer@exclusive.com</p>
-                <p>Emails: support@exclusive.com</p>
-                <form>
-                    <input type="text" name="first_name" placeholder="Enter your name" />
-                    <input type="email" name="email" placeholder="Enter your email" />
-                    <input type="number" name="phone" placeholder="Enter your Phone" />
-                    <input type="text area" name="message" placeholder="Enter your message" />
-                    <button>Send message</button>
-                </form>
-            </div>
-        </div>
+    return<div className="contact-form-container">
+    <div className="contact-info">
+      <div className="contact-item">
+        <h3>Call To Us</h3>
+        <p>We are available 24/7, 7 days a week.</p>
+        <p>Phone: +880611112222</p>
+      </div>
+      <hr></hr>
+      <div className="contact-item">
+        <h3>Write To Us</h3>
+        <p>Fill out our form and we will contact you within 24 hours.</p>
+        <p>Emails: customer@exclusive.com</p>
+        <p>Emails: support@exclusive.com</p>
+      </div>
+    </div>
+    <form className="contact-form">
+      <input
+        type="text"
+        name="name"
+        placeholder="Your Name *"
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Your Email *"
+         />
+      <input
+        type="tel"
+        name="phone"
+        placeholder="Your Phone *"
+         />
+      <textarea
+        name="message"
+        placeholder="Your Message">
+
+        </textarea>
+      <button type="submit">Send Message</button>
+    </form>
+  </div>
     }
 export default Contact;
--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,18 +1,24 @@
 
+import '../Styles/signup.css'
 function Signup() {
-    return <div className="container1">
-        <h1>Create an account!</h1>
-        <p>Enter your details below:</p>
-        <form>
-            <input type="text" name="Name" placeholder="Name" />
-            <br />
-            <input type="text" placeholder="Email or Phone Number" />
-            <br />
-            <input type="password" placeholder="Password" />
-            <br />
-            <button type="submit">Create Account</button>
-            <p>Already have an account? <a href="/login">Log In</a></p> 
-        </form>
-    </div>
+    return <div className="sign-up">
+    <h1>Create an account!</h1>
+    <p>Enter your details below:</p>
+    <form>
+      <div className="inputs-group">
+        <input type="text" name="Name" placeholder="Name" />
+      </div>
+      <div className="inputs-group">
+      <input type="text" placeholder="Email or Phone Number" />
+      </div>
+      <div className="inputs-group">
+      <input type="password" placeholder="Password" />
+      </div>
+      <button type="submit" className="sign-button">Create Account</button>
+      <p className="para">
+        Already have an account?<a href="/login">Log In</a>
+      </p>
+    </form>
+  </div>
 }
 export default Signup;
