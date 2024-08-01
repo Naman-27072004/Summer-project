c392f368c9fbd329bca4d8f0fabc5d030de02d84
Naman-27072004 <namangupta27072004@gmail.com>
2024-08-01 13:41:01 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(morgan('dev'));
 app.use('/api/v1/test',require("./routes/testRoutes"));
 
 //port
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 
 //listen
