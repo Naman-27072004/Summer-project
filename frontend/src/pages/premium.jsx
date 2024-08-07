2ad232f91c79739ef71f3b609b1ead374af4a015
Naman-27072004 <namangupta27072004@gmail.com>
2024-08-07 14:41:35 +0530

    first commit

--- frontend/src/pages/premium.jsx
+++ frontend/src/pages/premium.jsx
@@ -5,14 +5,14 @@ function Premium() {
             <h1>Membership Options</h1>
             <p>Choose the plan thats right for you.</p>
             <div className="plans_pp">
-                <div className="plan_pp">
+                {/* <div className="plan_pp">
                     <h2>Basic</h2>
                     <ul>
                         <li>✔ Feature 1</li>
                         <li>✔ Feature 2</li>
-                        <li>✔ Feature 3</li>
+                        <li>✔ Feature 3</li><hr/>
                     </ul>
-                    <p className="price_pp">$9.99<br /><span>per month</span></p>
+                    <p className="price_pp">₹900.99<br /><span>per month</span></p>
                     <button>Subscribe Now</button>
                 </div>
                 <div className="plan_pp">
@@ -21,21 +21,21 @@ function Premium() {
                         <li>✔ Feature 1</li>
                         <li>✔ Feature 2</li>
                         <li>✔ Feature 3</li>
-                        <li>✔ Feature 4</li>
+                        <li>✔ Feature 4</li><hr/>
                     </ul>
-                    <p className="price_pp">$19.99<br /><span>per month</span></p>
+                    <p className="price_pp">₹1900.99<br /><span>per month</span></p>
                     <button>Subscribe Now</button>
-                </div>
+                </div> */}
                 <div className="plan_pp">
-                    <h2>Unlimited</h2>
+                    <h2>Premium</h2>
                     <ul>
-                        <li>✔ Feature 1</li>
-                        <li>✔ Feature 2</li>
-                        <li>✔ Feature 3</li>
-                        <li>✔ Feature 4</li>
-                        <li>✔ Feature 5</li>
+                        <li>✔ Sell unlimited Products</li>
+                        <li>✔ Less transaction fees</li>
+                        <li>✔ 24/7 support</li>
+                        <li>✔ Discount codes</li>
+                        <li>✔ Multiple inventory locations</li><hr/>
                     </ul>
-                    <p className="price_pp">$29.99<br /><span>per month</span></p>
+                    <p className="price_pp">₹ 999<br /><span>per month</span></p>
                     <button>Subscribe Now</button>
                 </div>
             </div>
